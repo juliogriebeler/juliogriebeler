c13d46c3bafedf3e87b38840059add5b29540e0e
Julio Griebeler CH <julio.griebeler@ext.collectivehealth.com>
2021-08-23 17:22:23 -0300

    Removing unused stuff

--- web/images/colchao.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<h3>Fotos Colchao</h3>
-<?php
-$dirr = scandir("colchao");
-die($dirr[0] . " || " .$dirr[1] . " || " .$dirr[2] . " || " .$dirr[3] . " || " .$dirr[4] . " || " .$dirr[5] . " || " .$dirr[6] . " || " .$dirr[7]);
-?>
-</body>
-</html>
